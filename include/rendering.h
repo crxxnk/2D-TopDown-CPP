b31fb927b358fb7b3d70509b8c0479cf62bad5c7
crxxnk <135547044+crxxnk@users.noreply.github.com>
2024-08-19 12:30:22 +0300

    shit

--- include/rendering.h
+++ include/rendering.h
@@ -1,13 +1,14 @@
 #pragma once
+#include <raylib.h>
 #include "textures.h"
 #include "input.h"
-#include <raylib.h>
+
+int frameCount{};
 
 void Update(){
 	running = !WindowShouldClose();
 
-	playerSrc.x = 0;
-
+	playerSrc.x = playerSrc.width * float(playerFrame);
 	if(playerMoving)
 	{
 		if(playerUp) {playerDest.y-=playerSpeed;}
@@ -16,12 +17,16 @@ void Update(){
 		if(playerRight) {playerDest.x+=playerSpeed;}
 		if(frameCount % 8 == 1)
 			playerFrame++;
-		playerSrc.x = playerSrc.width * float(playerFrame);
-	}
+	} else if (frameCount % 45 == 1)
+		playerFrame++;
 
 	frameCount++;
 	if(playerFrame > 3)
 		playerFrame = 0;
+	if(!playerMoving && playerFrame > 1)
+		playerFrame = 0;
+
+	playerSrc.x = playerSrc.width * float(playerFrame);
 	playerSrc.y = playerSrc.height * float(playerDir);
 
 	camera.target = Vector2{float(playerDest.x-(playerDest.width/2)), float(playerDest.y-(playerDest.height/2))};
--- include/textures.h
+++ include/textures.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#include <fstream>
 
 Camera2D camera;
 
@@ -13,6 +14,12 @@ Color bgColor = Color{147, 211, 196, 255};
 Texture2D grassSprite;
 Texture2D playerSprite;
 
+Rectangle tileDest;
+Rectangle tileSrc;
+std::vector<int> tileMap{};
+std::string srcMap[]{};
+int mapW{}, mapH{};
+
 Rectangle playerSrc;
 Rectangle playerDest;
 
@@ -21,12 +28,31 @@ int playerDir{};
 bool playerUp, playerDown, playerLeft, playerRight;
 int playerFrame{};
 
-int frameCount{};
-
 void DrawScene()
 {
-	DrawTexture(grassSprite, 100, 50, WHITE);
+	// DrawTexture(grassSprite, 100, 50, WHITE);
+
+	for (size_t i = 0; i < tileMap.size(); i++) {
+		if(tileMap.at(i)!=0) {
+			tileDest.x = tileDest.width * float(i % mapW);
+			tileDest.y = tileDest.height * float(i / mapW);
+			tileSrc.x = tileSrc.width * float((tileMap.at(i)-1) % int(grassSprite.width/int(tileSrc.width)));
+			tileSrc.y = tileSrc.height * float((tileMap[i]-1) / int(grassSprite.width/int(tileSrc.width)));
+			DrawTexturePro(grassSprite, tileSrc, tileDest, Vector2{tileDest.width, tileDest.height}, 0, WHITE);
+		}
+	}
+
 	DrawTexturePro(playerSprite, playerSrc, playerDest, Vector2{playerDest.width, playerDest.height}, 0, WHITE);
+}
+
+void loadMap(std::string mapFile)
+{
+	std::ifstream map_file(mapFile);
+	mapW = 5;
+	mapH = 5;
+	for(int i = 0; i < (mapW*mapH); i++) {
+		tileMap.push_back(128);
+	}
 }
 
 void Init()
@@ -36,12 +62,18 @@ void Init()
 	SetTargetFPS(60);
 
 	grassSprite = LoadTexture("assets/SproutLands/Tilesets/Grass.png");
+
+	tileDest = Rectangle{0, 0, 16, 16};
+	tileSrc = Rectangle{0, 0, 16, 16};
+
 	playerSprite = LoadTexture("assets/Sproutlands/Characters/Basic Charakter Spritesheet.png");
 
 	playerSrc = Rectangle{0, 0, 48, 48};
 	playerDest = Rectangle{200, 200, 100, 100};
 
 	camera = Camera2D{Vector2{float(screenWidth/2), float(screenHeight/2)}, Vector2{float(playerDest.x-(playerDest.width/2)), float(playerDest.y-(playerDest.height/2))}, 0.0, 1.5};
+	
+	loadMap("grass.map");
 }
 
 void unloadTextures()
